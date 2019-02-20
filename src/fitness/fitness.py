9ca4e200da3f69277328dc5f4ea90df6fd44f8b7
Anna Bogdanova <ann.bogdanova@gmail.com>
2019-02-20 16:40:15 +0900

    Put parameters into text file

--- src/fitness/fitness.py
+++ src/fitness/fitness.py
@@ -14,17 +14,22 @@ class fitness(base_ff):
     def __init__(self):
         # Initialise base fitness function class.
         super().__init__()
+        self.max_nfe = params['EVALUATIONS']
+        self.dimension = params['DIMENSION']
+        self.my_func = eval(params['FUNCTION'])
+        self.bounds = params['BOUNDS']
 
     def evaluate(self, ind, **kwargs):    	
         # ind.phenotype will be a string, including function definitions etc.
         # When we exec it, it will create a value XXX_output_XXX, but we exec
         # inside an empty dict for safety.
+        par_dict = {"max_nfe": self.max_nfe, "dimension": self.dimension, "my_func": self.my_func, "bounds": self.bounds}
 
         p, d = ind.phenotype, {}
         
         # Exec the phenotype.
         try:
-            exec(p, d)
+            exec(p, par_dict, d)
             #print(p,  file=open(str(id(ind)), 'w'))
         except Exception as err:
             print(p)
