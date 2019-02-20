bd1694a1ebff83b9db6ede229581d35109850733
Anna Bogdanova <ann.bogdanova@gmail.com>
2019-02-20 15:15:35 +0900

    new_ge

--- src/fitness/fitness.py
+++ src/fitness/fitness.py
@@ -30,17 +30,17 @@ class fitness(base_ff):
         
         # Exec the phenotype.
         try:
-            # exec(p, d)
-            print(p,  file=open(str(id(ind)), 'w'))
+            exec(p, d)
+            #print(p,  file=open(str(id(ind)), 'w'))
         except Exception as err:
             print(p)
             print(err)
             raise err
 
         # Get the output
-        # s = d['XXX_output_XXX']  # this is the program's output: a number.
+        s = d['XXX_output_XXX']  # this is the program's output: a number.
         
-        return 1
+        #return 1
         return s
 
 #################Uncoment for shorter grammar
