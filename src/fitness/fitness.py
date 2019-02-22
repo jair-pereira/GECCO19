ffd03e5f20f2353e60e51cd3ea1642c48bc2c0b4
Anna Bogdanova <ann.bogdanova@gmail.com>
2019-02-22 12:17:34 +0900

    Restored original fitness code

--- src/fitness/fitness.py
+++ src/fitness/fitness.py
@@ -2,8 +2,6 @@ from algorithm.parameters import params
 from fitness.base_ff_classes.base_ff import base_ff
 import numpy as np
 from math import *
-import src
-import testFunctions as tf
 
 
 
@@ -14,23 +12,16 @@ class fitness(base_ff):
     def __init__(self):
         # Initialise base fitness function class.
         super().__init__()
-        self.max_nfe = params['EVALUATIONS']
-        self.dimension = params['DIMENSION']
-        self.my_func = eval(params['FUNCTION'])
-        self.bounds = params['BOUNDS']
 
-    def evaluate(self, ind, **kwargs):    	
+    def evaluate(self, ind, **kwargs):      
         # ind.phenotype will be a string, including function definitions etc.
         # When we exec it, it will create a value XXX_output_XXX, but we exec
         # inside an empty dict for safety.
-        par_dict = {"max_nfe": self.max_nfe, "dimension": self.dimension, "my_func": self.my_func, "bounds": self.bounds}
-
         p, d = ind.phenotype, {}
         
         # Exec the phenotype.
         try:
-            exec(p, par_dict, d)
-            #print(p,  file=open(str(id(ind)), 'w'))
+            exec(p, d)
         except Exception as err:
             print(p)
             print(err)
@@ -38,6 +29,7 @@ class fitness(base_ff):
 
         # Get the output
         s = d['XXX_output_XXX']  # this is the program's output: a number.
+        
         
         return s
 
