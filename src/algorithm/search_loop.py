26a76c7b8aac42eab7d62c5bfdad6af2177506c3
Jair Pereira <jair.pereira@aluno.ufabc.edu.br>
2019-03-12 18:28:04 +0900

    update for log

--- src/algorithm/search_loop.py
+++ src/algorithm/search_loop.py
@@ -55,7 +55,7 @@ def search_loop():
     :return: The final population after the evolutionary process has run for
     the specified number of generations.
     """
-    logf = open(params['EXPERIMENT_NAME']+"_log.csv", 'w') #190312: log
+    logf = open(params['EXPERIMENT_NAME2']+"_log.csv", 'w') #190312: log
     
     set_M()#190307: our mod for learning multiplier
     
