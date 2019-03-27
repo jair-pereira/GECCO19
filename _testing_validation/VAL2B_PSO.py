4703a64123712dac88dd2050a066eba8bbd00eb3
Jair Pereira <jair.pereira@aluno.ufabc.edu.br>
2019-03-27 17:50:17 +0900

    update validation and testing scripts

---
+++ _testing_validation/VAL2B_PSO.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+import sys, pickle, datetime
+import cocoex, cocopp
+from solvers import pso, de, cs, ga
+
+output_folder = "PSO_VAL2B_190327" #CHANGE HERE!
+nfe_base = 1e+5
+
+observer = cocoex.Observer("bbob", "result_folder: " + output_folder)
+
+suite = cocoex.Suite("bbob", "", "function_indices:16-29 dimensions:20 instance_indices:1-5")
+print("Experiment ", output_folder,  " started at ",datetime.datetime.now(), "\n on suite", suite)
+for problem in suite:
+    problem.observe_with(observer)
+    max_nfe = nfe_base*problem.dimension
+    
+    pso(200, problem, (problem.lower_bounds[0], problem.upper_bounds[0]), problem.dimension, max_nfe, w=0.32, c1=0.01, c2=1.54)
+    
+    print(problem.id, " finished at ",datetime.datetime.now())
+    
+cocopp.main(observer.result_folder)
