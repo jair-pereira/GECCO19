900c6ffd4bbcf605497c4d67eb0a092a2c383187
Jair Pereira <jair.pereira@aluno.ufabc.edu.br>
2019-03-27 18:04:47 +0900

    add GA

---
+++ _testing_validation/VAL2A_GA.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+import sys, pickle, datetime
+import cocoex, cocopp
+from solvers import pso, de, cs, ga
+
+output_folder = "GA_VAL2A_190327" #CHANGE HERE!
+nfe_base = 1e+5
+
+observer = cocoex.Observer("bbob", "result_folder: " + output_folder)
+
+suite = cocoex.Suite("bbob", "", "function_indices:2-5 dimensions:20 instance_indices:1-5")
+print("Experiment ", output_folder,  " started at ",datetime.datetime.now(), "\n on suite", suite)
+for problem in suite:
+    problem.observe_with(observer)
+    max_nfe = nfe_base*problem.dimension
+    
+    ga(400,  problem, (problem.lower_bounds[0], problem.upper_bounds[0]), problem.dimension, max_nfe, k=25, alpha=0.07, pr=0.79)
+    
+    print(problem.id, " finished at ",datetime.datetime.now())
+    
+cocopp.main(observer.result_folder)
